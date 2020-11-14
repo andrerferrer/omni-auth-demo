9105ba5f0f1cefe3052b57a125d094385599c079
andre ferrer <andrerferrer@hotmail.com>
2020-11-14 18:04:29 -0300

    Prepare the app to receive the callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,35 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
          :omniauthable
+
+  def self.find_for_oauth(auth)
+    # Create the user params
+    user_params = auth.slice("provider", "uid")
+    user_params.merge! auth.info.slice("email", "first_name", "last_name")
+    user_params[:picture_url] = auth.info.image
+    user_params[:token] = auth.credentials.token
+    user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
+    user_params = user_params.to_h
+    # Finish creating the user params
+
+    # Find the user if there was a log in
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+
+    # If the User did a regular sign up in the past, find it
+    user ||= User.find_by(email: auth.info.email)
+
+    # If we had a user, update it
+    if user
+      user.update(user_params)
+    # Else, create a new user with the params that come from the app callback
+    else
+      user = User.new(user_params)
+      # create a fake password for validation
+      user.password = Devise.friendly_token[0,20]
+      user.save
+    end
+
+    return user
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
