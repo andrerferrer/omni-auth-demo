12e58494316257c0d788a298d0e8f47624c2ea4f
andre ferrer <andrerferrer@hotmail.com>
2020-11-14 22:45:56 -0300

    add spotify to oAuth Controller (I forgot to add it hehe)

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,4 +10,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
+  def spotify
+    user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if user.persisted?
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Spotify') if is_navigational_format?
+    else
+      session['devise.spotify_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
 end
